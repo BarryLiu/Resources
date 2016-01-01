9597548624a2bd432b9aea34ba8aae7b96f66bb4
BarryLiu <1025587160@qq.com>
2016-01-01 17:27:34 +0800

    work8 很多bug

--- Work7-8-9(FileEditor)/src/com/barry/fileeditor/FileManager.java
+++ Work7-8-9(FileEditor)/src/com/barry/fileeditor/FileManager.java
@@ -1,29 +1,47 @@
 package com.barry.fileeditor;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Environment;
 
 public class FileManager {
-	 public static final String BACK_TO_ROOT = "返回到根目录";
-	 public static final String BACK_TO_UP   = "返回上一层";
-	
-	 public static String CopyPath;
-	 public static String CurrPath;
-	 
-	 public static List<FileBean> getFileLists(String path) {
-		 List<FileBean> datas =new ArrayList<FileBean>();
-		 
-		 if(!"/".equals(path)){
-			 FileBean btr= new FileBean("/", FileManager.BACK_TO_ROOT);
-			 datas.add(btr);
-			 
-			 FileBean btu =new FileBean(FileManager.CurrPath, FileManager.BACK_TO_UP);
-			 datas.add(btu);
-		 }
-		 
+	public static final String BACK_TO_ROOT = "返回到根目录";
+	public static final String BACK_TO_UP = "返回上一层";
+
+	public static String CopyPath;
+	public static String CurrPath;
+
+	private FileManager() {
+	}
+
+	private static FileManager fileManager;
+
+	public static FileManager getInstance() {
+		if (fileManager == null)
+			return new FileManager();
+		return fileManager;
+	}
+
+	public static List<FileBean> getFileLists(String path) {
+		List<FileBean> datas = new ArrayList<FileBean>();
+
+		if (!"/".equals(path)) {
+			FileBean btr = new FileBean("/", FileManager.BACK_TO_ROOT);
+			datas.add(btr);
+
+			FileBean btu = new FileBean(FileManager.CurrPath,
+					FileManager.BACK_TO_UP);
+			datas.add(btu);
+		}
+
 		File file = new File(path);
 		File[] listFiles = file.listFiles();
 		for (int i = 0; i < listFiles.length; i++) {
@@ -35,9 +53,76 @@ public class FileManager {
 		}
 		return datas;
 	}
-	private static String getSdCard(){
+
+	public static String getSdCard() {
 		return Environment.getRootDirectory().getPath();
 	}
-	 
-	 
+
+	public boolean deleteFile(File file) {
+		if (file.isDirectory()) {
+			File[] listFiles = file.listFiles();
+			for (int i = 0; i < listFiles.length; i++) {
+				deleteFile(listFiles[i]);
+			}
+			file.delete();
+		}
+		file.delete();
+		return false;
+	}
+
+	public static boolean createFile(File file) {
+		try {
+			return file.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static boolean createFolder(File file) {
+		return file.mkdir();
+	}
+
+	public boolean copyFile() {
+		File srcFile = new File(FileManager.CopyPath);
+		File tagFile = new File(FileManager.CurrPath);
+		try {
+			return copyFile(srcFile, tagFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private boolean copyFile(File srcFile, File tagFile)
+			throws FileNotFoundException {
+
+		if (srcFile.isDirectory()) {
+			tagFile.mkdir();
+			File[] listFiles = srcFile.listFiles();
+			for (int i = 0; i < listFiles.length; i++) {
+				File newFile = new File(tagFile.getPath(), srcFile.getName());
+				copyFile();
+			}
+			return true;
+		} else {
+			try {
+				InputStream is = new FileInputStream(srcFile);
+				OutputStream os = new FileOutputStream(tagFile);
+				byte[] buffer = new byte[1024];
+				int len = -1;
+				while ((len = is.read(buffer)) != -1) {
+					os.write(buffer, 0, len);
+				}
+				os.close();
+				is.close();
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+
 }
--- Work7-8-9(FileEditor)/src/com/barry/fileeditor/MainActivity.java
+++ Work7-8-9(FileEditor)/src/com/barry/fileeditor/MainActivity.java
@@ -7,12 +7,18 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -62,36 +68,138 @@ public class MainActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				FileAdapter adapter = (FileAdapter) parent.getAdapter();
-				FileBean fb= (FileBean) adapter.getItem(position);
-				
+				FileBean fb = (FileBean) adapter.getItem(position);
+
 				File file = new File(fb.getFilePath());
-				if(file.canRead()){
-					if(file.isDirectory())
+				if (file.canRead()) {
+					if (file.isDirectory())
 						initData(file.getPath());
 					else
 						showTips("不是文件夹不能打打开");
-				}else{
+				} else {
 					showTips("没有读取的权限");
 				}
-				
+
 			}
-
-			
 		});
+		
+		lv_listView.setOnItemLongClickListener(new OnItemLongClickListener() {
 
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				FileAdapter adapter = (FileAdapter) parent.getAdapter();
+				final FileBean fileBean = (FileBean) adapter.getItem(position);
+				final File file=new File(fileBean.getFilePath());
+				Builder builder =new Builder(MainActivity.this);
+				builder.setTitle("操作");
+				builder.setItems(new String[]{"复制","删除"}, new OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface arg0, int witch) {
+						switch (witch) {
+						case 0://复制
+							FileManager.CopyPath = FileManager.CurrPath;
+							showTips("复制");
+							break;
+						case 1://删除
+							if(FileManager.getInstance().deleteFile(file))
+								showTips("删除成功");
+							else
+								showTips("删除失败");
+							break;
+						}
+					}
+				}).show();
+				return true;
+			}
+		});
+		
+		
+		gv_menu.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				switch (position) {
+				case 0://点击手机
+					initData("/");
+					break;
+				case 1:
+					String sdCardPath = FileManager.getSdCard();
+					if(sdCardPath==null)
+						showTips("sd卡不存在");
+					else
+						initData(sdCardPath);
+					break;
+				case 2://新建
+					Builder builder =new Builder(MainActivity.this);
+					builder.setTitle("操作");
+					final View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.dailog_layout, null);
+					builder.setView(v);
+					
+					builder.setNegativeButton("取消", null);
+					builder.setPositiveButton("确定", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							TextView tv_name = (TextView) v.findViewById(R.id.tv_name);
+							RadioGroup rg_new =(RadioGroup) v.findViewById(R.id.rg_new);
+							File file=new File(FileManager.CurrPath,tv_name.getText().toString());
+							boolean flag=true;
+							switch (rg_new.getCheckedRadioButtonId()) {
+							case R.id.rb_file:
+								flag=FileManager.createFile(file);
+								break;
+							case R.id.rb_folder:
+								flag=FileManager.createFolder(file);
+								break;
+							}
+							if(flag)
+								showTips("创建成功");
+							else
+								showTips("创建失败");
+						}
+					});
+					builder.show();
+					break;
+				case 3://粘贴
+					if(FileManager.getInstance().copyFile())
+						showTips("粘贴成功");
+					else
+						showTips("粘贴失败");
+					break;
+				case 4://退出
+					Builder builder2 =new Builder(MainActivity.this);
+					 builder2.setTitle("退出吗？");
+					 builder2.setPositiveButton("取消", null).setNegativeButton("确定", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							finish();
+						}
+					}).show();
+					break;
+ 
+				}
+			}
+		});
 	}
+
 	private void showTips(String string) {
-		Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();;
+		Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+		;
 	}
+
 	private void initData(String path) {
-	
-		datas=FileManager.getFileLists(path);
+
+		datas = FileManager.getFileLists(path);
 
 		FileAdapter adapter = new FileAdapter(datas, MainActivity.this);
 		lv_listView.setAdapter(adapter);
-		
+
 		tv_path.setText(path);
-		FileManager.CurrPath=path;
+		FileManager.CurrPath = path;
 	}
 
 	/** 找到控件并为其赋值 */
